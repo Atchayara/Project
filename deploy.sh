9cb1d4d3a9249e3288a5696c03d74fc036d94d77
Atchayara <151494352+Atchayara@users.noreply.github.com>
2024-02-10 20:26:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,13 @@ GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$GIT_BRANCH" = "dev" ]; then
 
-	docker push atchayara/dev:latest
+        docker tag atchayara/reactapp:latest dev:devdeploy
+	docker push atchayara/dev:devdeploy
 
 elif [ "$GIT_BRANCH" = "master" ]; then
 
-	docker push atchayara/production:latest
+	docker tag atchayara/reactapp:latest prod:proddeploy
+	docker push atchayara/production:proddeploy
 else
     echo "Not on a deployable branch (dev or production). Deployment aborted."
     exit 1
