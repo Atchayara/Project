e5993781afb6d0b130f7bf742885b7ba68e5f963
Atchayara <151494352+Atchayara@users.noreply.github.com>
2024-02-11 10:23:47 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,18 @@
 
 docker-compose up -d
 
-docker tag reactapp:latest atchayara/reactapp:dep
-
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$GIT_BRANCH" = "dev" ]; then
 
-        docker tag reactapp:dep atchayara/reactdev:devdeploy
-	docker push dev/reactapp:dep
+        docker tag reactapp:latest atchayara/dev:latest
+	docker push atchayara/dev:latest
 
 elif [ "$GIT_BRANCH" = "master" ]; then
 
-	docker tag reactapp:dep atchayara/reactprod:proddeploy
-	docker push production/reactapp:dep
+        docker tag reactapp:latest atchayara/production:latest
+	docker push atchayara/production:latest
 else
     echo "Not on a deployable branch (dev or master). Deployment aborted."
     exit 1
